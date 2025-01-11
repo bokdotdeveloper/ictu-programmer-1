840494cf4350cc01edb856c7940320fb979c05c1
summertime-saga <ronegualan@gmail.com>
2025-01-11 09:59:59 +0800

    update links2

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -9,7 +9,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({origin: "https://kasubay-ai.vercel.app/", credentials: true}));
+app.use(cors({origin: "https://kasubay-ai.vercel.app", credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
