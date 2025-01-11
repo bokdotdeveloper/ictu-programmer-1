70151970d838ccafe5a5579cb6f91ed223664fa0
summertime-saga <ronegualan@gmail.com>
2025-01-11 09:02:05 +0800

    update v4

---
+++ backend/api/index.ts
@@ -0,0 +1,33 @@
+/*
+import app from "./app.js"
+import { connectToDatabase } from "./db/connection.js";
+
+//connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(()=>{
+  app.listen(5000, ()=> console.log("Server Open and Connected to the Database 👌"));
+})
+.catch((error)=>console.log(error));
+*/
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+let isDbConnected = false;
+
+export default async function handler(req, res) {
+  try {
+    // Connect to the database only once
+    if (!isDbConnected) {
+      await connectToDatabase();
+      isDbConnected = true;
+      console.log("Database Connected 👌");
+    }
+
+    // Forward the request to the app
+    app(req, res);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
